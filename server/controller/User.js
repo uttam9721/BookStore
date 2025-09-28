8261451a546ce592ac2c0c99d88668337e1946cc
uttam9721 <uttammaurya377@gmail.com>
2025-09-29 00:34:39 +0530

    auth done

--- server/controller/User.js
+++ server/controller/User.js
@@ -1,13 +1,13 @@
-// import {User} from '../models/UserModel.js'
 import { User } from "../models/UserModel.js";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const register=async(req,res)=>{
-    const {name,email,password}=req.body;
+    let {name,email,password}=req.body;
     try {
-      const user=await User.findOne({email}) 
+      let user=await User.findOne({email}) 
       if (user) return res.json({message:'user already register',success:false});
-      const hashPass=await bcrypt.hash(password,10);
+      let hashPass=await bcrypt.hash(password,10);
         user =await User.create({
         name,
         email,
@@ -15,7 +15,35 @@ export const register=async(req,res)=>{
     })
     res.status(201).json({message:'User register Successfully',success:true,user})
     } catch (error) {
-        res.status(404).json({message:'user not found '});
-        
+        res.status(404).json({message:'user not found '});     
     }
 }
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'User not found', success: false });
+    }
+
+    let validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ message: 'Invalid credentials', success: false });
+    }
+
+    let token =await jwt.sign({ userId: user._id }, "uttammaurya", {
+      expiresIn: "365d",
+    });
+
+    res.status(200).json({
+      message: `Welcome ${user.name}`,
+      token,
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { register } from '../controller/User.js';
+import { login, register } from '../controller/User.js';
 
 const router=express.Router();
 
 
 router.post('/register',register);
+router.post('/login',login);
 
 export default router;
